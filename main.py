44bce15b7b7caa8977837d602070f75512af888e
jonobri1@gmail.com <jonobri1@gmail.com>
2024-01-13 16:26:50 +1100

    add database insert and checking

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sqlite3
+import os.path
+
 from functions import download_pdf, read_pdf, parse_pdf, send_email
+import database as db
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
@@ -11,7 +15,13 @@ def processor(council: dict):
   
   print(f'Running {council_name} scraper...')
   download_link = council['scraper']
-  print(download_link)
+
+  if not download_link:
+    print(f'No link found for {council_name}.')
+    return
+  if db.check_link(download_link):
+    print(f'Link already scraped for {council_name}.')
+    return
   
   print('Link scraped! Downloading PDF...')
   download_pdf(download_link, council_name)
@@ -21,11 +31,16 @@ def processor(council: dict):
   
   print('PDF read! Parsing PDF...')
   matches = parse_pdf(regex_list, text)  
-  print(matches)
   
-  print('PDF parsed! Sending email...')
+  print('PDF parsed! Inserting into database...')
+  db.insert(council_name, download_link, matches)
+  print('Database updated!')
+  
+  print('Sending email...')
   if config['GMAIL_FUNCTIONALITY'] == 1: 
-    send_email(config['GMAIL_ACCOUNT_RECEIVE'], f'New agenda: {council_name}', str(matches))
+    send_email(config['GMAIL_ACCOUNT_RECEIVE'], 
+               f'New agenda: {council_name}', 
+               str(matches))
     print(f'Email sent!')
   else:
     print(f"Email functionality is disabled.")
@@ -34,6 +49,9 @@ def processor(council: dict):
   
 
 def main():
+  if not os.path.exists('./agendas.db'):
+    db.init()
+  
   processor(maribyrnong)
   
 if __name__ == '__main__':
