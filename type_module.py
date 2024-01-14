16e6f9ebac178fd4dc6c97ff514dbee98027bd07
jonobri1@gmail.com <jonobri1@gmail.com>
2024-01-14 16:10:11 +1100

    Comment annotations: Council TypedDict

--- type_module.py
+++ type_module.py
@@ -1,7 +1,7 @@
 from typing import TypedDict, Callable
 
 class Council(TypedDict):
-  council: str
-  regex_dict: dict
-  scraper: Callable
+  council: str # Name of council (e.g. Maribyrnong; Merri-bek)
+  regex_dict: dict # Dictionary of regex types and regexes
+  scraper: Callable # Function that returns a link to the agenda
   
