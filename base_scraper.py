6213283b0e152589adda3cc3eec6f5914ee4b1ab
jonobri1@gmail.com <jonobri1@gmail.com>
2024-02-05 07:38:26 +1100

    document BaseScraper class

--- base_scraper.py
+++ base_scraper.py
@@ -14,6 +14,29 @@ def register_scraper(cls):
 
 
 class BaseScraper:
+    """
+    Base class for all council scrapers.
+
+    Attributes:
+        `DEFAULTHEADERS (dict)`: Default headers for the requests.
+        `council_name (str)`: Name of the council to scrape (snake_case).
+        `state (str)`: State of the council.
+        `base_url (str)`: Base URL for the council's website.
+        `logger (logging.Logger)`: Logger instance for the scraper.
+        `session (requests.Session)`: Session object for making requests.
+        `driver (selenium.webdriver.Chrome)`: Selenium WebDriver instance.
+
+    Methods:
+        `log(level, message, *args)`: Logs a message with the provided level, including council name and state.
+        `set_headers(headers)`: Sets the headers for the session.
+        `setup_selenium_driver()`: Sets up a Selenium WebDriver instance.
+        `get_selenium_driver()`: Returns the Selenium WebDriver instance, setting it up if necessary.
+        `fetch_with_requests(url, method="GET", **kwargs)`: Fetches a URL with the requests module.
+        `fetch_with_selenium(url, wait_time=10, wait_condition=None)`: Fetches a URL with Selenium, optionally waiting for a condition.
+        `scraper()`: Abstract method for scraping the council's website. Must be implemented by subclasses.
+        `close()`: Closes the Selenium WebDriver instance if it exists.
+    """
+
     DEFAULTHEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         "Accept-Language": "en-US,en;q=0.9",
