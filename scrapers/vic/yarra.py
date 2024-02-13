a008b9086d5df5a71031232cc1d922028cda027d
nickylauw <nickylauw3@gmail.com>
2024-02-13 20:22:39 +1100

    added yarra scraper

--- scrapers/vic/yarra.py
+++ scrapers/vic/yarra.py
@@ -9,6 +9,7 @@ from base_scraper import BaseScraper, register_scraper
 from logging.config import dictConfig
 from _dataclasses import ScraperReturn
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 import re
 
 
@@ -17,29 +18,52 @@ class YarraScraper(BaseScraper):
     def __init__(self):
         council = "yarra"
         state = "VIC"
-        base_url = ""
+        base_url = "https://www.yarracity.vic.gov.au"
         super().__init__(council, state, base_url)
 
     def scraper(self) -> ScraperReturn | None:
-        # self.logger.info(f"Starting {self.council_name} scraper")
+        self.logger.info(f"Starting {self.council_name} scraper")
+        initial_webpage_url = "https://www.yarracity.vic.gov.au/about-us/council-and-committee-meetings/upcoming-council-and-committee-meetings"
 
-        self.logger.error(f"{self.council_name} is without a Scraper, Can you Help!")
-        return None
-        """
-        YOUR CODE HERE
-        """
+        output = self.fetch_with_requests(initial_webpage_url)
+        output = output.content
+        print(output)
 
-        # scraper_return = ScraperReturn(name, date, time, self.base_url, download_url)
 
-        # self.logger.info(f"""
-        #     {scraper_return.name}
-        #     {scraper_return.date}
-        #     {scraper_return.time}
-        #     {scraper_return.webpage_url}
-        #     {scraper_return.download_url}"""
-        # )
-        # self.logger.info(f"{self.council_name} scraper finished successfully")
-        # return scraper_return
+        name = None
+        date = None
+        time = None
+        download_url = None
+        agenda_link = None
+
+        # finds agenda link
+        initial_soup = BeautifulSoup(output, "html.parser")
+        agenda_list = initial_soup.find('div', class_='show-for-medium-up')
+        agenda_link = agenda_list.find('a')['href']
+        agenda_output = self.fetch_with_requests(agenda_link)
+        agenda_output = agenda_output.content
+
+        # takes name, date, download url from agenda link
+        soup = BeautifulSoup(agenda_output, "html.parser")
+
+        name = soup.find('h1', class_='heading').text
+        date = soup.find('p', class_='lead').text
+        date = date.strip()
+        download_url = soup.find('a', class_='download-link')['href']
+        download_url = self.base_url + download_url
+
+        scraper_return = ScraperReturn(name, date, time, self.base_url, download_url)
+
+        self.logger.info(
+            f"""
+                    {scraper_return.name} 
+                    {scraper_return.date} 
+                    {scraper_return.time} 
+                    {scraper_return.webpage_url} 
+                    {scraper_return.download_url}"""
+        )
+        self.logger.info(f"{self.council_name} scraper finished successfully")
+        return scraper_return
 
 
 if __name__ == "__main__":
