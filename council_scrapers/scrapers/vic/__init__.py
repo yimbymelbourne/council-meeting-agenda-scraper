8c0f49c25abd2c05d23680e4a1ba070e74945d8b
Michael Clayden <michaelclayden00@gmail.com>
2024-02-15 15:57:40 +1100

    Update state folders' __init__ files

--- council_scrapers/scrapers/vic/__init__.py
+++ council_scrapers/scrapers/vic/__init__.py
@@ -3,7 +3,7 @@ from council_scrapers.scrapers.vic.darebin import DarebinScraper
 from council_scrapers.scrapers.vic.glen_eira import GlenEiraScraper
 from council_scrapers.scrapers.vic.melbourne import MelbourneScraper
 from council_scrapers.scrapers.vic.merribek import MerribekScraper
-from council_scrapers.scrapers.vic.monash import MonashScraper
+
 from council_scrapers.scrapers.vic.port_phillip import PortPhilipScraper
 from council_scrapers.scrapers.vic.whitehorse import WhitehorseScraper
 from council_scrapers.scrapers.vic.yarra import YarraScraper
@@ -14,4 +14,5 @@ from council_scrapers.scrapers.vic.yarra import YarraScraper
 # UNIMPLEMENTED SCRAPERS
 # from council_scrapers.scrapers.vic.stonnington import StonningtonScraper
 # from council_scrapers.scrapers.vic.manningham import ManninghamScraper
+# from council_scrapers.scrapers.vic.monash import MonashScraper
 # from council_scrapers.scrapers.vic.kingston import KingstonScraper
