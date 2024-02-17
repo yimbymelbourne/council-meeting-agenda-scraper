b08a75fee82543b2536ce6f74c3a7cdb7712f3a1
jonobri1@gmail.com <jonobri1@gmail.com>
2024-02-18 00:11:25 +1100

    fix kuringgai base url

--- council_scrapers/scrapers/nsw/kuringgai.py
+++ council_scrapers/scrapers/nsw/kuringgai.py
@@ -6,6 +6,6 @@ class KuRingGaiScraper(InfoCouncilScraper):
     def __init__(self):
         council = "kuringgai"
         state = "NSW"
-        base_url = "http://www.krg.nsw.gov.au/"
+        base_url = "http://eservices.kmc.nsw.gov.au/"
         infocouncil_url = "https://eservices.kmc.nsw.gov.au/Infocouncil.Web/"
         super().__init__(council, state, base_url, infocouncil_url)
