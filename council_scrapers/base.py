c48def05a1e32a58d427ecbdd082d4d671f4916d
Michael Clayden <42601907+ArchangelEkim@users.noreply.github.com>
2024-02-24 13:27:25 +1100

    Update base.py
    Update Chrome version in DEFAULTHEADERS to stable version of Chrome 120.

--- council_scrapers/base.py
+++ council_scrapers/base.py
@@ -80,7 +80,7 @@ class DefaultFetcher(Fetcher):
         self.__driver = None
 
     DEFAULTHEADERS = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.62 Safari/537.3",
         "Accept-Language": "en-US,en;q=0.9",
         "Referer": "https://www.google.com/",
         "Connection": "keep-alive",
