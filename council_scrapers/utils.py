30bb73fbc3fa4aa6cb6743023db22c4164c64972
Michael Clayden <michaelclayden00@gmail.com>
2024-03-17 17:25:39 +1100

    Add function to create and write ical into byte string.

--- council_scrapers/utils.py
+++ council_scrapers/utils.py
@@ -4,8 +4,11 @@ import os.path
 
 import fitz
 import smtplib
+import pytz
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from icalendar import Calendar, Event
+from datetime import datetime
 
 from base import ScraperReturn
 
@@ -41,7 +44,7 @@ def write_email(
 ) -> str:
     email_body = f"Hello,\n\nThe agenda for the {scraper_result.date} {council_name} meeting is now available for download.\n\nPlease click on the link below to download the agenda:\n{scraper_result.download_url}\n\n"
 
-    if parser_results["keyword_matches"]:
+    if parser_results:
         email_body += "Here are the matches found in the agenda:\n"
         email_body += "\nKeyword matches:\n"
         for regex, count in parser_results.items():
@@ -51,6 +54,20 @@ def write_email(
 
     return email_body
 
+def write_ical_event(council_name: str,
+                     scraper_result: ScraperReturn,
+                     ):
+    cal = Calendar()
+    cal.add("prodid", "-//Agenda Scraper//morehomes.au//")
+    cal.add("version", "2.0")
+
+    event = Event()
+    event.add("summary", f"{council_name} Upcoming Meeting Agenda")
+    eventdate = datetime.strptime(scraper_result.date)
+    event.add("dstart", datetime.combine(eventdate, datetime.strptime(scraper_result.time), pytz.timezone('Australia/Sydney')))
+    
+    cal.add_component(event)
+    return cal.to_ical()
 
 def send_email(to, subject, body):
     if int(config["GMAIL_FUNCTIONALITY"]) == 1:
