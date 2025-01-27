a4ca54a377f6950d3dd70401296f2146af06a838
davidmac76 <davidmac26@gmail.com>
2025-01-27 13:24:40 +1100

    Comply with black format

--- aus_council_scrapers/scrapers/nsw/wollongong.py
+++ aus_council_scrapers/scrapers/nsw/wollongong.py
@@ -6,7 +6,7 @@ from datetime import datetime
 @register_scraper
 class WollongongScraper(BaseScraper):
     def __init__(self):
-        print('Wollongong')
+        print("Wollongong")
         council_name = "wollongong"
         state = "NSW"
         base_url = "https://www.wollongong.nsw.gov.au/"
@@ -15,7 +15,7 @@ class WollongongScraper(BaseScraper):
     def scraper(self) -> ScraperReturn | None:
         self.logger.info(f"Starting {self.council_name} scraper")
 
-        business_papers_url = 'https://www.wollongong.nsw.gov.au/council/council-meetings/councilbusinesspapers'
+        business_papers_url = "https://www.wollongong.nsw.gov.au/council/council-meetings/councilbusinesspapers"
 
         name = None
         date = None
@@ -29,53 +29,58 @@ class WollongongScraper(BaseScraper):
         initial_soup = BeautifulSoup(output, "html.parser")
 
         # Find the table with class 'buspaper'
-        buspaper_table = initial_soup.find('table', class_='buspaper')
+        buspaper_table = initial_soup.find("table", class_="buspaper")
 
         # Table should exist, so throw error if it doesn't
         if not buspaper_table:
             self.logger.info(f"{self.council_name} Business paper table not found")
-            raise ValueError("The buspaper_table with class 'buspaper' was not found in the HTML content.")
+            raise ValueError(
+                "The buspaper_table with class 'buspaper' was not found in the HTML content."
+            )
 
         # Extract rows from the buspaper_table
-        rows = buspaper_table.find_all('tr')[1:]  # Skip the header row if it exists
+        rows = buspaper_table.find_all("tr")[1:]  # Skip the header row if it exists
 
         # Extract agendas from business papers table
         agendas = []
         for row in rows:
-            cells = row.find_all('td')
+            cells = row.find_all("td")
 
             # Skip rows where the 3rd column is not 'Agenda'
-            if cells[2].get_text(strip=True) != 'Agenda':
+            if cells[2].get_text(strip=True) != "Agenda":
                 continue
 
             # Extract the meeting date, agenda name, and URL
-            meeting_date = datetime.strptime(cells[1].get_text(strip=True), '%d %b %Y')
-            link = cells[3].find('a')
+            meeting_date = datetime.strptime(cells[1].get_text(strip=True), "%d %b %Y")
+            link = cells[3].find("a")
             agenda_name = link.get_text(strip=True)
-            agenda_url = link.get('href')
+            agenda_url = link.get("href")
 
             # Append a dictionary with the extracted data to the agendas list
-            agendas.append({
-                'meeting_date': meeting_date,
-                'agenda_name': agenda_name,
-                'agenda_url': agenda_url
-            })
+            agendas.append(
+                {
+                    "meeting_date": meeting_date,
+                    "agenda_name": agenda_name,
+                    "agenda_url": agenda_url,
+                }
+            )
 
         # Get agenda with most recent meeting date
         # TODO: this gets most recent agenda even if it is in the past. Should it only
         # get agenda in the future?
-        most_recent_agenda = max(agendas, key=lambda x: x['meeting_date'])
+        most_recent_agenda = max(agendas, key=lambda x: x["meeting_date"])
 
-        name = most_recent_agenda['agenda_name']
-        date = most_recent_agenda['meeting_date'].strftime('%Y-%m-%d')
-        download_url = most_recent_agenda['agenda_url']
+        name = most_recent_agenda["agenda_name"]
+        date = most_recent_agenda["meeting_date"].strftime("%Y-%m-%d")
+        download_url = most_recent_agenda["agenda_url"]
 
-        scraper_return = ScraperReturn(name,
-                                       date,
-                                       time,
-                                       self.base_url,
-                                       download_url,
-                                       )
+        scraper_return = ScraperReturn(
+            name,
+            date,
+            time,
+            self.base_url,
+            download_url,
+        )
 
         self.logger.info(
             f"""
